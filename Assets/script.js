b576d9bb1a1b7c2686ecf7720e904ddf8f943052
Mark Anthony <touya1313@hotmail.com>
2023-06-30 01:26:08 -0500

    finished questions in JS

--- Assets/script.js
+++ Assets/script.js
@@ -13,4 +13,117 @@ let scoreCount = 0;
 let count = 16
 let countdown; 
 
-// 10 questions
+// 10 questions array
+
+const quizArray = [
+    {
+        id: "0",
+        question: "What is the capital of Mexico?",
+        options: [
+            "DF",
+            "Mexico City",
+            "Mexico",
+            "All of the above",
+        ],
+        correct: "All of the above",
+    },
+    {
+        id: "1",
+        question: "What is the capital of Ethiopia?",
+        options: [
+            "Ankara",
+            "Alofi",
+            "Addis Ababa",
+            "Antananarivo",
+        ],
+        correct: "Addis Ababa",
+    },
+    {
+        id: "2",
+        question: "What is the capital of Kyrgyzstan?",
+        options: [
+            "Bishkek",
+            "Bucharest",
+            "Belgrade",
+            "Pretoria",
+        ],
+        correct: "Bishkek",
+    },
+    {
+        id: "3",
+        question: "What is the capital of Qatar?",
+        options: [
+            "Damascus",
+            "Djibouti",
+            "Dakar",
+            "Doha",
+        ],
+        correct: "Doha",
+    },
+    {
+        id: "4",
+        question: "What is the capital of Vietnam?",
+        options: [
+            "Saigon",
+            "Hanoi",
+            "Ho-Chi-Minh City",
+            "Da Nang",
+        ],
+        correct: "Hanoi",
+    },
+    {
+        id: "5",
+        question: "What is the capital of Slovenia?",
+        options: [
+            "Lusaka",
+            "Ljubljana",
+            "Luanda",
+            "Lisbon",
+        ],
+        correct: "Ljubljana",
+    },
+    {
+        id: "6",
+        question: "What is the capital of Cyprus?",
+        options: [
+            "Thessaloniki",
+            "Oia",
+            "Nicosia",
+            "Naxos",
+        ],
+        correct: "Nicosia",
+    },
+    {
+        id: "7",
+        question: "What is the capital of Chile?",
+        options: [
+            "San Jose",
+            "San Juan",
+            "Santiago",
+            "Sofia",
+        ],
+        correct: "Santiago",
+    },
+    {
+        id: "8",
+        question: "What is the capital of Georgia?",
+        options: [
+            "Tallinn",
+            "Tirana",
+            "Tiraspol",
+            "Tbilisi",
+        ],
+        correct: "Tbilisi",
+    },
+    {
+        id: "9",
+        question: "What is the capital of Malta?",
+        options: [
+            "Vaduz",
+            "Victoria",
+            "Valletta",
+            "Vilnius",
+        ],
+        correct: "Valletta",
+    },
+];
