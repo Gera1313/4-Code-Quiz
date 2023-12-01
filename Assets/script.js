a281dffd49cfa2fe00d84e7cd982e24b417d75b9
Mark Anthony <touya1313@hotmail.com>
2023-11-30 21:06:36 -0600

    formatted using prettier. worked on view score modal

--- Assets/script.js
+++ Assets/script.js
@@ -10,7 +10,7 @@ let startScreen = document.querySelector(".start-screen");
 let startButton = document.getElementById("start-button");
 let questionCount;
 let scoreCount = 0;
-let countdown; 
+let countdown;
 
 let saveScoreBtn = document.getElementById("save-score");
 let viewScoresBtn = document.getElementById("view-scores");
@@ -19,263 +19,224 @@ let finalScoreDisplay = document.getElementById("final-score");
 // 10 questions array
 
 const quizArray = [
-    {
-        id: "0",
-        question: "What is the capital of Mexico?",
-        options: [
-            "DF",
-            "Mexico City",
-            "Mexico",
-            "All of them",
-        ],
-        correct: "All of them",
-    },
-    {
-        id: "1",
-        question: "What is the capital of Ethiopia?",
-        options: [
-            "Ankara",
-            "Alofi",
-            "Addis Ababa",
-            "Antananarivo",
-        ],
-        correct: "Addis Ababa",
-    },
-    {
-        id: "2",
-        question: "What is the capital of Kyrgyzstan?",
-        options: [
-            "Bishkek",
-            "Bucharest",
-            "Belgrade",
-            "Pretoria",
-        ],
-        correct: "Bishkek",
-    },
-    {
-        id: "3",
-        question: "What is the capital of Qatar?",
-        options: [
-            "Damascus",
-            "Djibouti",
-            "Dakar",
-            "Doha",
-        ],
-        correct: "Doha",
-    },
-    {
-        id: "4",
-        question: "What is the capital of Vietnam?",
-        options: [
-            "Saigon",
-            "Hanoi",
-            "Ho-Chi-Minh City",
-            "Da Nang",
-        ],
-        correct: "Hanoi",
-    },
-    {
-        id: "5",
-        question: "What is the capital of Slovenia?",
-        options: [
-            "Lusaka",
-            "Ljubljana",
-            "Luanda",
-            "Lisbon",
-        ],
-        correct: "Ljubljana",
-    },
-    {
-        id: "6",
-        question: "What is the capital of Cyprus?",
-        options: [
-            "Thessaloniki",
-            "Oia",
-            "Nicosia",
-            "Naxos",
-        ],
-        correct: "Nicosia",
-    },
-    {
-        id: "7",
-        question: "What is the capital of Chile?",
-        options: [
-            "San Jose",
-            "San Juan",
-            "Santiago",
-            "Sofia",
-        ],
-        correct: "Santiago",
-    },
-    {
-        id: "8",
-        question: "What is the capital of Georgia?",
-        options: [
-            "Tallinn",
-            "Tirana",
-            "Tiraspol",
-            "Tbilisi",
-        ],
-        correct: "Tbilisi",
-    },
-    {
-        id: "9",
-        question: "What is the capital of Malta?",
-        options: [
-            "Vaduz",
-            "Victoria",
-            "Valletta",
-            "Vilnius",
-        ],
-        correct: "Valletta",
-    },
+  {
+    id: "0",
+    question: "What is the capital of Mexico?",
+    options: ["DF", "Mexico City", "Mexico", "All of them"],
+    correct: "All of them",
+  },
+  {
+    id: "1",
+    question: "What is the capital of Ethiopia?",
+    options: ["Ankara", "Alofi", "Addis Ababa", "Antananarivo"],
+    correct: "Addis Ababa",
+  },
+  {
+    id: "2",
+    question: "What is the capital of Kyrgyzstan?",
+    options: ["Bishkek", "Bucharest", "Belgrade", "Pretoria"],
+    correct: "Bishkek",
+  },
+  {
+    id: "3",
+    question: "What is the capital of Qatar?",
+    options: ["Damascus", "Djibouti", "Dakar", "Doha"],
+    correct: "Doha",
+  },
+  {
+    id: "4",
+    question: "What is the capital of Vietnam?",
+    options: ["Saigon", "Hanoi", "Ho-Chi-Minh City", "Da Nang"],
+    correct: "Hanoi",
+  },
+  {
+    id: "5",
+    question: "What is the capital of Slovenia?",
+    options: ["Lusaka", "Ljubljana", "Luanda", "Lisbon"],
+    correct: "Ljubljana",
+  },
+  {
+    id: "6",
+    question: "What is the capital of Cyprus?",
+    options: ["Thessaloniki", "Oia", "Nicosia", "Naxos"],
+    correct: "Nicosia",
+  },
+  {
+    id: "7",
+    question: "What is the capital of Chile?",
+    options: ["San Jose", "San Juan", "Santiago", "Sofia"],
+    correct: "Santiago",
+  },
+  {
+    id: "8",
+    question: "What is the capital of Georgia?",
+    options: ["Tallinn", "Tirana", "Tiraspol", "Tbilisi"],
+    correct: "Tbilisi",
+  },
+  {
+    id: "9",
+    question: "What is the capital of Malta?",
+    options: ["Vaduz", "Victoria", "Valletta", "Vilnius"],
+    correct: "Valletta",
+  },
 ];
 
 restart.addEventListener("click", () => {
-    initial();
-    displayContainer.classList.remove("hide");
-    scoreContainer.classList.add("hide");
+  initial();
+  displayContainer.classList.remove("hide");
+  scoreContainer.classList.add("hide");
 });
 
 nextBtn.addEventListener("click", () => {
-    questionCount += 1;
+  questionCount += 1;
 
-    if (questionCount === quizArray.length) {
-        displayContainer.classList.add("hide");
-        userScore.innerHTML = "Your Score is " +
-        scoreCount + " out of " + questionCount;
-    } else {
-        countOfQuestion.innerHTML = questionCount + 1 + " of " + quizArray.length + " Question";
-        quizDisplay(questionCount);
+  if (questionCount === quizArray.length) {
+    displayContainer.classList.add("hide");
+    userScore.innerHTML =
+      "Your Score is " + scoreCount + " out of " + questionCount;
+  } else {
+    countOfQuestion.innerHTML =
+      questionCount + 1 + " of " + quizArray.length + " Question";
+    quizDisplay(questionCount);
 
-        count = 11;
-        clearInterval(countdown);
-        timerDisplay();
-    }
-});
-
-const timerDisplay = () => {
-    countdown = setInterval(() => {
-        count--;
-        timeLeft.innerHTML= `${count}s`;
-        if (count === 0) {
-            clearInterval(countdown);
-            displayNext();
-        }
-    }, 1000);
-}
-
-const quizDisplay = (questionCount) => {
-    let quizCards = document.querySelectorAll(".container-mid");
-
-    quizCards.forEach((card) => {
-        card.classList.add("hide");
-    });
-    quizCards[questionCount].classList.remove("hide");
-};
-
-function quizCreator() {
-    quizArray.sort(() => Math.random() - 0.5);
-
-    for (let i of quizArray) {
-        i.options.sort(() => Math.random() - 0.5);
-        let div = document.createElement("div");
-        div.classList.add("container-mid", "hide");
-
-        countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Questions ";
-
-        let question_DIV = document.createElement("p");
-        question_DIV.classList.add("question");
-        question_DIV.innerHTML = i.question; 
-        div.appendChild(question_DIV);
-
-        div.innerHTML += `<button class="option-div" onclick="checker(this)"> ${i.options[0]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[1]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[2]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[3]}</button>`
-        quizContainer.appendChild(div);
-    }
-}
-
-function checker(userOption){
-    let userSolution = userOption.innerText;
-
-    if(userSolution === quizArray[questionCount].correct){
-        userOption.classList.add("correct");
-        scoreCount++;
-    }
-    else {
-        count -= 3; // deducts seconds if wrong answer
-        userOption.classList.add("incorrect");
-
-        options.forEach((element) => {
-            if ((element.innerText = quizArray[questionCount].correct)){
-                element.classList.add("correct");
-            }
-
-        });
-    }
-    clearInterval(countdown);
-    options.forEach((element) => {
-        element.disabled = true;
-    });
-}
-
-function initial() {
-    quizContainer.innerHTML = "";
-    questionCount = 0;
-    scoreCount = 0;
     count = 11;
     clearInterval(countdown);
     timerDisplay();
-    quizCreator();
-    quizDisplay(questionCount);
+  }
+});
+
+const timerDisplay = () => {
+  countdown = setInterval(() => {
+    count--;
+    timeLeft.innerHTML = `${count}s`;
+    if (count === 0) {
+      clearInterval(countdown);
+      displayNext();
+    }
+  }, 1000);
+};
+
+const quizDisplay = (questionCount) => {
+  let quizCards = document.querySelectorAll(".container-mid");
+
+  quizCards.forEach((card) => {
+    card.classList.add("hide");
+  });
+  quizCards[questionCount].classList.remove("hide");
+};
+
+function quizCreator() {
+  quizArray.sort(() => Math.random() - 0.5);
+
+  for (let i of quizArray) {
+    i.options.sort(() => Math.random() - 0.5);
+    let div = document.createElement("div");
+    div.classList.add("container-mid", "hide");
+
+    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Questions ";
+
+    let question_DIV = document.createElement("p");
+    question_DIV.classList.add("question");
+    question_DIV.innerHTML = i.question;
+    div.appendChild(question_DIV);
+
+    div.innerHTML += `<button class="option-div" onclick="checker(this)"> ${i.options[0]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[1]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[2]}</button><br><button class="option-div" onclick="checker(this)"> ${i.options[3]}</button>`;
+    quizContainer.appendChild(div);
+  }
 }
 
-startButton.addEventListener("click", () =>{
-    startScreen.classList.add("hide");
-    displayContainer.classList.remove("hide");
-    initial();
+function checker(userOption) {
+  let userSolution = userOption.innerText;
+
+  if (userSolution === quizArray[questionCount].correct) {
+    userOption.classList.add("correct");
+    scoreCount++;
+  } else {
+    count -= 3; // deducts seconds if wrong answer
+    userOption.classList.add("incorrect");
+
+    options.forEach((element) => {
+      if ((element.innerText = quizArray[questionCount].correct)) {
+        element.classList.add("correct");
+      }
+    });
+  }
+  clearInterval(countdown);
+  options.forEach((element) => {
+    element.disabled = true;
+  });
+}
+
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 11;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
+
+startButton.addEventListener("click", () => {
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
 });
 
 window.onload = () => {
-    startScreen.classList.remove("hide");
-    displayContainer.classList.add("hide");
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
 };
 
 // Handles saving the score to local storage when the user clicks the Save Score button
 
 saveScoreBtn.addEventListener("click", () => {
-    saveScore();
+  saveScore();
 });
 
 function saveScore() {
-    let initials = document.getElementById("initials-input").value.toUpperCase();
-    if (initials.trim() === "") {
-        alert("Please enter your initials.");
-        return;
-    }
+  let initials = document.getElementById("initials-input").value.toUpperCase();
+  if (initials.trim() === "") {
+    alert("Please enter your initials.");
+    return;
+  }
 
-    // Creates object to store score and initials
-    let scoreData = {
-        score: scoreCount,
-        initials: initials,
-    };
+  // Creates object to store score and initials
+  let scoreData = {
+    score: scoreCount,
+    initials: initials,
+  };
 
-    // Retrieve scores from local storage
-    let existingScores = JSON.parse(localStorage.getItem("scores")) || [];
+  // Retrieve scores from local storage
+  let existingScores = JSON.parse(localStorage.getItem("scores")) || [];
 
-    //  Add new score to the array
-    existingScores.push(scoreData);
+  //  Add new score to the array
+  existingScores.push(scoreData);
 
-    // Save the updated scores back to local storage
-    localStorage.setItem("scores", JSON.stringify(existingScores));
+  // Save the updated scores back to local storage
+  localStorage.setItem("scores", JSON.stringify(existingScores));
 
-    // Hides quiz container and shows score page
-    displayContainer.classList.add("hide");
-    scoreContainer.classList.add("hide");
-    document.querySelector(".score-page").classList.remove("hide");
+  // Hides quiz container and shows score page
+  displayContainer.classList.add("hide");
+  scoreContainer.classList.add("hide");
+  document.querySelector(".score-page").classList.remove("hide");
 
-    // Displays the final score
-    finalScoreDisplay.innerText = scoreCount;
+  // Displays the final score
+  finalScoreDisplay.innerText = scoreCount;
 }
 
 // Handles displaying the score when the user clicks the View Score button
+viewScoresBtn.addEventListener("clicks", () => {
+  document.getElementById("scores-overlay").classList.remove("hide");
+  displayScores();
+});
 
+document.getElementById("close-scores").addEventListener("click", () => {
+  document.getElementById("scores-overlay").classList.add("hide");
+});
+
+function displayScores() {
+  // retrieve scores from local storage
+  let scores = JSON.parse(localStorage.getItem("scores")) || [];
+}
